993b275e5ac8647172f7facae07d5a907c27cf75
André Bastos <afcjb@protonmail.com>
2021-06-21 08:33:14 +0100

    App has now search, sort and pagination functionalities.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createContext } from "react";
+import AppBar from "./AppBar";
+import ImageContainer from "./ImageContainer";
+import Pagination from "./Pagination";
+import { connect } from "react-redux";
+import * as actions from "./redux/actions.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { namePrefix: "", sorted: false };
+  }
+
+  componentDidMount = () => {
+    const { namePrefix } = this.state;
+    const { sorted } = this.state;
+    const { newSearch } = this.props;
+    const { getCityImages } = this.props;
+    newSearch(namePrefix, sorted);
+    getCityImages();
+  };
+
+  handleSearch = (e) => {
+    this.setState({ namePrefix: e.target.value });
+    console.log(this.props);
+    console.log(this.state);
+  };
+
+  toggleSorted = (e) => {
+    this.setState((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.checked };
+    });
+  };
+
+  render() {
+    const { namePrefix } = this.state;
+    const { sorted } = this.state;
+    const { cities } = this.props;
+    const { data } = cities;
+    const { links } = cities;
+    const { metadata } = cities;
+    const { images } = this.props;
+    const { newSearch } = this.props;
+
+    return (
+      <>
+        <AppBar
+          namePrefix={namePrefix}
+          sorted={sorted}
+          handleSearch={this.handleSearch}
+          toggleSorted={this.toggleSorted}
+          newSearch={newSearch}
+        ></AppBar>
+        <ImageContainer data={data} images={images}></ImageContainer>
+        <Pagination links={links} metadata={metadata} newSearch={newSearch} />
+      </>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    cities: state.cities,
+    details: state.details,
+    images: state.images,
+  };
+};
+
+export default connect(mapStateToProps, actions)(App);
